731ffb261047ec984540aee0ef4f340e476799f3
Kobutaemon <isotoa1219@gmail.com>
2024-01-11 16:09:36 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function genPass() {
+    var passBase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()_{}";
+    var passLen = document.querySelector(".input-len");
+    
+    alert(passLen.value);
+    // for(var i = 0; i < len)
+  }
