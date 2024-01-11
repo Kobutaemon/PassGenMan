4ed0ae5c893641caa515170d6089e61aa4beb6ae
Kobutaemon <isotoa1219@gmail.com>
2024-01-11 17:18:14 +0900

    再生成ボタンの実装

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+var passBase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()_{}"; //67文字
+var password = "";
+
+
 function genPass() {
-    var passBase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()_{}";
+
+    var cFilled = document.querySelector(".input-password");
+
+    if (cFilled.value == "") {
+        var passLen = document.querySelector(".input-len");
+
+        
+        password = "";        
+
+        //パスワード生成
+        for (var i = 0; i < passLen.value; i++) {
+            password += passBase.charAt(Math.floor(Math.random() * passBase.length))
+        };
+
+        cFilled.value = password;
+
+    } else {
+        recordPass();
+    }
+
+};
+
+
+function reGen() {
+
+    var cFilled = document.querySelector(".input-password");
     var passLen = document.querySelector(".input-len");
-    
-    alert(passLen.value);
-    // for(var i = 0; i < len)
-  }
+
+    password = "";
+        
+    //パスワード生成
+    for (var n = 0; n < passLen.value; n++) {
+        password += passBase.charAt(Math.floor(Math.random() * (passBase.length + 1)))
+    };
+
+    cFilled.value = password;
+}
+
+function recordPass() {
+    return;
+    //ファイルにパスワードを書き込む
+}
